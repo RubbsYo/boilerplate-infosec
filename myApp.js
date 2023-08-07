3d3bf4818c545bed27e6a1636ca40c3fbfd805c8
RubbsYo <mikurubbs@gmail.com>
2023-08-07 00:57:37 -0500

    test 7

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet')
 const app = express();
-
+var timeInSeconds = 90*24*60*60;
 
 
 app.use(helmet.hidePoweredBy());
@@ -9,6 +9,7 @@ app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
 
 
 
